9f6cf2328dc522cb66b7c36f175f763369744a8b
a.arh <a.arh@iz.ru>
2025-05-29 11:28:18 +0300

    2.1.5

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const injectedScript = document.createElement('script');
-injectedScript.src = chrome.runtime.getURL('js/injected.js');
-(document.head || document.documentElement).appendChild(injectedScript);
+//const injectedScript = document.createElement('script');
+//#injectedScript.src = chrome.runtime.getURL('js/injected.js');
+//(document.head || document.documentElement).appendChild(injectedScript);
 
 window.aBlock = {
 
